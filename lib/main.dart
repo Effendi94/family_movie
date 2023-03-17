7c0bb8bb5fef7b428ed3d6acddebdca6fa5f3436
Effendi Nugraha <effendi94.autobot@gmail.com>
2023-03-17 13:45:09 +0700

    add setup locator & snackbar ui

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import 'application/app/app.locator.dart';
 import 'application/app/family_movie_app.dart';
+import 'ui/customs/custom_snackbar/setup_snackbar_ui.dart';
 import 'flavors.dart';
 
 void main() {
@@ -19,9 +21,9 @@ void main() {
     ),
   );
 
-  // setupLocator();
+  setupLocator();
   // setupBottomSheetUI();
-  // setupSnackbarUI();
+  setupSnackbarUI();
 
   runApp(const FamilyMovieApp());
 }
