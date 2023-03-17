8f2f93dcae787743a7ab6ec56b1b2018aa4b8371
Effendi Nugraha <effendi94.autobot@gmail.com>
2023-03-17 13:39:14 +0700

    create api servuce and dio interceptor

---
+++ lib/apis/api_interceptor.dart
@@ -0,0 +1,120 @@
+import 'dart:developer' as developer;
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+// import 'package:stacked_services/stacked_services.dart';
+
+// import '../../application/app/app.locator.dart';
+// import '../../application/app/app.router.dart';
+// import '../../application/app/constants/common.dart';
+// import '../../application/app/constants/image_constants.dart';
+// import '../ui/views/shared/custom_loader.dart';
+
+class ApiInterceptor extends InterceptorsWrapper {
+  // final _bottomSheetService = locator<BottomSheetService>();
+  // final _navigationService = locator<NavigationService>();
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    log('============================================================\n');
+    log('==============================================onRequest=====\n');
+    developer.log(options.baseUrl, name: 'baseUrl');
+    developer.log(options.path, name: 'path');
+    developer.log(options.headers.toString(), name: 'headers');
+    developer.log(options.queryParameters.toString(), name: 'queryParameters');
+    developer.log(options.data.toString(), name: 'data');
+    log('============================================================\n');
+
+    super.onRequest(options, handler);
+  }
+
+  @override
+  void onResponse(response, ResponseInterceptorHandler handler) {
+    developer.log('success response', name: response.requestOptions.path);
+
+    super.onResponse(response, handler);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    developer.log(err.toString(), name: err.requestOptions.path);
+
+    // if (err.response!.statusCode == Common.sessionExpiredStatusCode) {
+    //   showSessionExpiredBottomSheet();
+    // } else if (err.response!.statusCode == Common.badGatewayStatusCode) {
+    //   showServiceUnavailableBottomSheet();
+    //   return;
+    // } else if (err.response!.statusCode == Common.invalidCredentialStatusCode) {
+    //   showInvalidCredentialBottomSheet();
+    //   return;
+    // } else if (err.response!.statusCode == Common.serviceUnavailable) {
+    //   showServiceUnavailableBottomSheet();
+    //   return;
+    // }
+
+    super.onError(err, handler);
+  }
+
+  // Future showServiceUnavailableBottomSheet() async {
+  //   CustomLoader.hideLoader();
+
+  //   await _bottomSheetService.showCustomSheet(
+  //     variant: BottomSheetType.generalError,
+  //   );
+  // }
+
+  // Future showInvalidCredentialBottomSheet() async {
+  //   CustomLoader.hideLoader();
+  //   await _bottomSheetService.showCustomSheet(
+  //     variant: BottomSheetType.base,
+  //     imageUrl: ImageConstants.generalError,
+  //     title: 'Sesi Anda sudah habis',
+  //     description:
+  //         'Anda akan kembali ke Beranda. Harap ulangi buka tabungan untuk mendapatkan akses kembali ya!',
+  //     barrierDismissible: false,
+  //     data: {
+  //       'showCornerClearButton': false,
+  //     },
+  //     mainButtonTitle: 'Oke',
+  //   );
+
+  //   if (_userService.isFromRayaApps) {
+  //     onCloseWebview();
+  //   } else {
+  //     _navigationService.clearStackAndShow(Routes.landingView);
+  //   }
+  // }
+
+  Future showSessionExpiredBottomSheet() async {
+    // await _bottomSheetService.showCustomSheet(
+    //   variant: BottomSheetType.base,
+    //   imageUrl: ImageConstants.unauthorized,
+    //   title: 'Sesi kamu habis',
+    //   description:
+    //       'Akun dikeluarkan secara otomatis karena tidak ada aktivitas. Login lagi untuk masuk ke akunmu ya!',
+    //   mainButtonTitle: 'Oke',
+    //   data: {
+    //     'showCornerClearButton': true,
+    //   },
+    // );
+
+    // SharedPrefHelper.removeKeys();
+    // LocatorHelper.unregThenRegViewModels();
+
+    // _navigationService.clearStackAndShow(Routes.welcomeBackView);
+  }
+
+  Future showSystemMaintenanceBottomSheet() async {
+    // await _bottomSheetService.showCustomSheet(
+    //   variant: BottomSheetType.base,
+    //   imageUrl: ImageConstants.systemMaintenance,
+    //   title: 'Lagi pemeliharaan sistem Raya',
+    //   description:
+    //       'Demi kenyamanan kamu menggunakan aplikasi Raya, saat ini tim kami sedang melakukan pemeliharaan sistem. Kembali lagi nanti, ya!',
+    //   data: {
+    //     'showCornerClearButton': true,
+    //   },
+    //   mainButtonTitle: 'Oke',
+    // );
+  }
+}
---
+++ lib/apis/movie_api.dart
@@ -0,0 +1,345 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+import '../application/app/app.locator.dart';
+import '../application/app/constants/common.dart';
+import '../application/app/constants/endpoint.dart';
+import '../application/helpers/dio_error_type_parser.dart';
+import '../application/models/movie.dart';
+import '../application/models/response_json.dart';
+import '../application/models/static.dart';
+import '../application/models/video_data.dart';
+import '../application/services/user_service.dart';
+
+class MovieAPI {
+  final Dio _dio = locator<Dio>();
+  final _userService = locator<UserService>();
+
+  Future<Either<ResponseJSON, Movie>> fetchNowPlaying(int page) async {
+    try {
+      final Map<String, dynamic> headers = {
+        'content-type': 'application/json',
+      };
+      final Map<String, dynamic> params = {
+        'api_key': _userService.movieAPIKey,
+        'page': page,
+        'language': 'en-US',
+      };
+      final res = await _dio.get(
+        EndPoint.nowPlayingMovie,
+        queryParameters: params,
+        options: Options(
+          headers: headers,
+        ),
+      );
+      if (res.statusCode == 200) {
+        return Right(Movie.fromJson(res.data));
+      } else {
+        return Left(ResponseJSON.fromJson(res.data));
+      }
+    } on DioError catch (e) {
+      final errorMessage = DioErrorTypeParser.parseToCustomMessage(e);
+      return Left(
+        ResponseJSON(
+          statusCode: e.response?.statusCode ?? Common.internalServerError,
+          statusMessage: errorMessage,
+          success: false,
+        ),
+      );
+    }
+  }
+
+  Future<Either<ResponseJSON, Movie>> fetchPopular(int page) async {
+    try {
+      final Map<String, dynamic> headers = {
+        'content-type': 'application/json',
+      };
+      final Map<String, dynamic> params = {
+        'api_key': _userService.movieAPIKey,
+        'page': page,
+        'language': 'en-US',
+      };
+      final res = await _dio.get(
+        EndPoint.popularMovie,
+        queryParameters: params,
+        options: Options(
+          headers: headers,
+        ),
+      );
+      if (res.statusCode == 200) {
+        return Right(Movie.fromJson(res.data));
+      } else {
+        return Left(ResponseJSON.fromJson(res.data));
+      }
+    } on DioError catch (e) {
+      final errorMessage = DioErrorTypeParser.parseToCustomMessage(e);
+      return Left(
+        ResponseJSON(
+          statusCode: e.response?.statusCode ?? Common.internalServerError,
+          statusMessage: errorMessage,
+          success: false,
+        ),
+      );
+    }
+  }
+
+  Future<Either<ResponseJSON, Movie>> fetchTopRated(int page) async {
+    try {
+      final Map<String, dynamic> headers = {
+        'content-type': 'application/json',
+      };
+      final Map<String, dynamic> params = {
+        'api_key': _userService.movieAPIKey,
+        'page': page,
+        'language': 'en-US',
+      };
+      final res = await _dio.get(
+        EndPoint.topRatedMovie,
+        queryParameters: params,
+        options: Options(
+          headers: headers,
+        ),
+      );
+      if (res.statusCode == 200) {
+        return Right(Movie.fromJson(res.data));
+      } else {
+        return Left(ResponseJSON.fromJson(res.data));
+      }
+    } on DioError catch (e) {
+      final errorMessage = DioErrorTypeParser.parseToCustomMessage(e);
+      return Left(
+        ResponseJSON(
+          statusCode: e.response?.statusCode ?? Common.internalServerError,
+          statusMessage: errorMessage,
+          success: false,
+        ),
+      );
+    }
+  }
+
+  Future<Either<ResponseJSON, Movie>> fetchUpcoming(int page) async {
+    try {
+      final Map<String, dynamic> headers = {
+        'content-type': 'application/json',
+      };
+      final Map<String, dynamic> params = {
+        'api_key': _userService.movieAPIKey,
+        'page': page,
+        'language': 'en-US',
+      };
+      final res = await _dio.get(
+        EndPoint.upcomingMovie,
+        queryParameters: params,
+        options: Options(
+          headers: headers,
+        ),
+      );
+      if (res.statusCode == 200) {
+        return Right(Movie.fromJson(res.data));
+      } else {
+        return Left(ResponseJSON.fromJson(res.data));
+      }
+    } on DioError catch (e) {
+      final errorMessage = DioErrorTypeParser.parseToCustomMessage(e);
+      return Left(
+        ResponseJSON(
+          statusCode: e.response?.statusCode ?? Common.internalServerError,
+          statusMessage: errorMessage,
+          success: false,
+        ),
+      );
+    }
+  }
+
+  Future<Either<ResponseJSON, Movie>> fetchMovieByCategory(
+      String endpoint, int page) async {
+    try {
+      final Map<String, dynamic> headers = {
+        'content-type': 'application/json',
+      };
+      final Map<String, dynamic> params = {
+        'api_key': _userService.movieAPIKey,
+        'page': page,
+        'language': 'en-US',
+      };
+      final res = await _dio.get(
+        endpoint,
+        queryParameters: params,
+        options: Options(
+          headers: headers,
+        ),
+      );
+      if (res.statusCode == 200) {
+        return Right(Movie.fromJson(res.data));
+      } else {
+        return Left(ResponseJSON.fromJson(res.data));
+      }
+    } on DioError catch (e) {
+      final errorMessage = DioErrorTypeParser.parseToCustomMessage(e);
+      return Left(
+        ResponseJSON(
+          statusCode: e.response?.statusCode ?? Common.internalServerError,
+          statusMessage: errorMessage,
+          success: false,
+        ),
+      );
+    }
+  }
+
+  Future<Either<ResponseJSON, MovieData>> fetchMovieDetail(
+      String movieId) async {
+    try {
+      final Map<String, dynamic> headers = {
+        'content-type': 'application/json',
+      };
+      final Map<String, dynamic> params = {
+        'api_key': _userService.movieAPIKey,
+        'language': 'en-US',
+      };
+      final res = await _dio.get(
+        EndPoint.detailMovie.replaceAll('%MOVIE_ID%', movieId),
+        queryParameters: params,
+        options: Options(
+          headers: headers,
+        ),
+      );
+      if (res.statusCode == 200) {
+        return Right(MovieData.fromJson(res.data));
+      } else {
+        return Left(ResponseJSON.fromJson(res.data));
+      }
+    } on DioError catch (e) {
+      final errorMessage = DioErrorTypeParser.parseToCustomMessage(e);
+      return Left(
+        ResponseJSON(
+          statusCode: e.response?.statusCode ?? Common.internalServerError,
+          statusMessage: errorMessage,
+          success: false,
+        ),
+      );
+    }
+  }
+
+  Future<Either<ResponseJSON, List<VideoData>>> fetchVideo(
+      String movieId) async {
+    try {
+      final Map<String, dynamic> headers = {
+        'content-type': 'application/json',
+      };
+      final Map<String, dynamic> params = {
+        'api_key': _userService.movieAPIKey,
+        'language': 'en-US',
+      };
+      final res = await _dio.get(
+        EndPoint.videoMovie.replaceAll('%MOVIE_ID%', movieId),
+        queryParameters: params,
+        options: Options(
+          headers: headers,
+        ),
+      );
+      if (res.statusCode == 200) {
+        final resultData = List<VideoData>.from(
+          List.from(res.data['results'])
+              .map(
+                (x) => VideoData.fromJson(x),
+              )
+              .toList(),
+        );
+        return Right(resultData);
+      } else {
+        return Left(ResponseJSON.fromJson(res.data));
+      }
+    } on DioError catch (e) {
+      final errorMessage = DioErrorTypeParser.parseToCustomMessage(e);
+      return Left(
+        ResponseJSON(
+          statusCode: e.response?.statusCode ?? Common.internalServerError,
+          statusMessage: errorMessage,
+          success: false,
+        ),
+      );
+    }
+  }
+
+  Future<Either<ResponseJSON, List<StaticModel>>> searchVideoByKeywords(
+      String query) async {
+    try {
+      final Map<String, dynamic> headers = {
+        'content-type': 'application/json',
+      };
+      final Map<String, dynamic> params = {
+        'api_key': _userService.movieAPIKey,
+        'query': query,
+      };
+      final res = await _dio.get(
+        EndPoint.searchByKeyword,
+        queryParameters: params,
+        options: Options(
+          headers: headers,
+        ),
+      );
+      if (res.statusCode == 200) {
+        final resultData = List<StaticModel>.from(
+          List.from(res.data['results'])
+              .map(
+                (x) => StaticModel.fromJson(x),
+              )
+              .toList(),
+        );
+        return Right(resultData);
+      } else {
+        return Left(ResponseJSON.fromJson(res.data));
+      }
+    } on DioError catch (e) {
+      final errorMessage = DioErrorTypeParser.parseToCustomMessage(e);
+      return Left(
+        ResponseJSON(
+          statusCode: e.response?.statusCode ?? Common.internalServerError,
+          statusMessage: errorMessage,
+          success: false,
+        ),
+      );
+    }
+  }
+
+  Future<Either<ResponseJSON, List<MovieData>>> searchMovie(
+      String query) async {
+    try {
+      final Map<String, dynamic> headers = {
+        'content-type': 'application/json',
+      };
+      final Map<String, dynamic> params = {
+        'api_key': _userService.movieAPIKey,
+        'query': query,
+      };
+      final res = await _dio.get(
+        EndPoint.searchMovie,
+        queryParameters: params,
+        options: Options(
+          headers: headers,
+        ),
+      );
+      if (res.statusCode == 200) {
+        final resultData = List<MovieData>.from(
+          List.from(res.data['results'])
+              .map(
+                (x) => MovieData.fromJson(x),
+              )
+              .toList(),
+        );
+        return Right(resultData);
+      } else {
+        return Left(ResponseJSON.fromJson(res.data));
+      }
+    } on DioError catch (e) {
+      final errorMessage = DioErrorTypeParser.parseToCustomMessage(e);
+      return Left(
+        ResponseJSON(
+          statusCode: e.response?.statusCode ?? Common.internalServerError,
+          statusMessage: errorMessage,
+          success: false,
+        ),
+      );
+    }
+  }
+}
