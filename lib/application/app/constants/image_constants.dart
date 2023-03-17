6e8a62492b193019ed42216a34b2f37a2cd7a3a2
Effendi Nugraha <effendi94.autobot@gmail.com>
2023-03-17 14:42:08 +0700

    add flutter image

--- lib/application/app/constants/image_constants.dart
+++ lib/application/app/constants/image_constants.dart
@@ -4,5 +4,6 @@ class ImageConstants {
 
   static String get bgDrawer => '$assetImage/bg_drawer.png';
   static String get avatar => '$assetImage/profile.png';
+  static String get flutterIcon => '$assetImage/flutter_icon.png';
   static String get loader => '$assetImage/loader.gif';
 }
