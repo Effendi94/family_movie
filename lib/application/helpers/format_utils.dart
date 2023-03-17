023a16d2ce3f5283d046c6e582be47981520fbde
Effendi Nugraha <effendi94.autobot@gmail.com>
2023-03-17 13:40:56 +0700

    create helpers

---
+++ lib/application/helpers/format_utils.dart
@@ -0,0 +1,24 @@
+import 'package:intl/intl.dart';
+
+class FormatUtils {
+  static double convertToDouble(dynamic value) {
+    if (value is String) {
+      return double.parse(value);
+    } else if (value is int) {
+      return 0.00 + value;
+    } else if (value == null) {
+      return 0.0;
+    } else {
+      return value;
+    }
+  }
+
+  static String convertShortDateTime(String strDate) {
+    if (strDate.isNotEmpty) {
+      DateTime dateTime = DateTime.parse(strDate);
+      String formattedDate = DateFormat('MMM dd, yyyy').format(dateTime);
+      return formattedDate;
+    }
+    return "";
+  }
+}
