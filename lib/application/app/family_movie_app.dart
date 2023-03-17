1901742283fa726c1f6427a4f4cde2bc673a56db
Effendi Nugraha <effendi94.autobot@gmail.com>
2023-03-17 13:41:36 +0700

    update main dart file

---
+++ lib/application/app/family_movie_app.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+// import 'app.router.dart';
+import 'constants/common.dart';
+import 'theme/app_theme_data.dart';
+
+class FamilyMovieApp extends StatelessWidget {
+  const FamilyMovieApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: Common.titleBar,
+      theme: appThemeData(context),
+      navigatorKey: StackedService.navigatorKey,
+      // onGenerateRoute: StackedRouter().onGenerateRoute,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      supportedLocales: const [
+        Locale('id'),
+      ],
+    );
+  }
+}
