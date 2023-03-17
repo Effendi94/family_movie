8e53557c06f76d971a95ca8cf89a305fd058978e
Effendi Nugraha <effendi94.autobot@gmail.com>
2023-03-17 13:40:26 +0700

    default test widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,15 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:family_movie/application/app/family_movie_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:family_movie/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const FamilyMovieApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
